1277624b45bc3cbf4780c5d711ef2afc406a5aff
pedro <progerios@hotmail.com>
2025-02-01 09:49:29 -0300

    fix(installer): include missing static files and ensure views are properly copied

--- sohocontrol_bootstrap/server.js
+++ sohocontrol_bootstrap/server.js
@@ -1,31 +1,38 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const app = express();
 const port = 4200;
 
-// Middleware para servir arquivos estáticos
-app.use(express.static(__dirname));
+// Corrige o caminho dos arquivos no executável pkg
+const basePath = process.pkg ? path.dirname(process.execPath) : __dirname;
 
-// Roteamento das URLs amigáveis
-app.get('/clientes', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
-});
+// Middleware para servir arquivos estáticos corretamente
+app.use('/assets', express.static(path.join(basePath, 'assets')));
+app.use('/lib', express.static(path.join(basePath, 'lib')));
+app.use('/js', express.static(path.join(basePath, 'js')));
+app.use('/fonts', express.static(path.join(basePath, 'assets', 'fonts')));
 
-app.get('/vendas', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'vendas.html'));
-});
+// Roteamento das páginas HTML (corrigido para funcionar no executável)
+const serveHTML = (route, filename) => {
+  app.get(route, (req, res) => {
+    const filePath = path.join(basePath, 'views', filename);
+    if (fs.existsSync(filePath)) {
+      res.sendFile(filePath);
+    } else {
+      res.status(404).send('Arquivo não encontrado.');
+    }
+  });
+};
 
-app.get('/estoque', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'produtos.html'));
-});
-
-app.get('/relatorios', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'relatorios.html'));
-});
+serveHTML('/clientes', 'index.html');
+serveHTML('/vendas', 'vendas.html');
+serveHTML('/estoque', 'produtos.html');
+serveHTML('/relatorios', 'relatorios.html');
 
 // Redirecionar todas as outras rotas para a página inicial
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+  res.sendFile(path.join(basePath, 'views', 'index.html'));
 });
 
 // Iniciar o servidor
